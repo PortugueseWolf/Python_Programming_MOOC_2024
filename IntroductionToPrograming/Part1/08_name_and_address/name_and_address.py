d36bd47935e18965c1cb2b1513631bc6488b8caa
PortugueseWolf <danieljdferreira@hotmail.com>
2023-10-17 21:40:18 +0100

    Part1 08_name_and_address completed

---
+++ IntroductionToPrograming/Part1/08_name_and_address/name_and_address.py
@@ -0,0 +1,11 @@
+# Ask user for details
+name = input("Given name:")
+familyName = input("Family name:")
+address = input("Street address:")
+cityAndPostal = input("City and postal code:")
+
+# Print details
+print(name + " " + familyName)
+print(address)
+print(cityAndPostal)
+
