578e7d791141db80e6b1b74bb7329adfda156aed
PortugueseWolf <125361522+PortugueseWolf@users.noreply.github.com>
2024-09-03 12:47:38 +0000

    Done

---
+++ IntroductionToPrograming/Part2/07_alphabetically_last/alphabetically_last.py
@@ -0,0 +1,9 @@
+first_word = input("Please type in the 1st word: ")
+second_word = input("Please type in the 2st word: ")
+
+if first_word > second_word:
+    print(f"{first_word} comes alphabetically last.")
+elif second_word > first_word:
+    print(f"{second_word} comes alphabetically last.")
+else:
+    print("You gave the same word twice.")
