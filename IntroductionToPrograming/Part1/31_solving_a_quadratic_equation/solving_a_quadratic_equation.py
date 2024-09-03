74c4f3a9cfe8eb564045d18452208593f4304d23
PortugueseWolf <125361522+PortugueseWolf@users.noreply.github.com>
2024-09-03 09:49:58 +0000

    Part 1 done

--- IntroductionToPrograming/Part1/31_solving_a_quadratic_equation/solving_a_quadratic_equation.py
+++ IntroductionToPrograming/Part1/31_solving_a_quadratic_equation/solving_a_quadratic_equation.py
@@ -4,9 +4,9 @@ a = int(input("Value of a: "))
 b = int(input("Value of b: "))
 c = int(input("Value of c: "))
 
-x = (-b + sqrt(b** - 4 * a * c))//2*a
-xx = (-b - sqrt(b** - 4 * a * c))//2*a
+x = (-b + sqrt(b*b - (4 * a * c)))/(2*a)
+y = (-b - sqrt(b*b - (4 * a * c)))/(2*a)
 
 print()
 
-print(f"The roots are {x} and {xx}")
+print(f"The roots are {x} and {y}")
