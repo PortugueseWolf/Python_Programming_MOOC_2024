07edd1972d6fe1afc702bcc324e2bac266bfbc7e
PortugueseWolf <danieljdferreira@gmail.com>
2024-09-15 17:55:59 +0100

    Done

--- IntroductionToPrograming/Part3/20_find_the_first_substring/find_the_first_substring.py
+++ IntroductionToPrograming/Part3/20_find_the_first_substring/find_the_first_substring.py
@@ -2,4 +2,6 @@ word = input("Please type in a word: ")
 character = input("Please type in a character: ")
 
 if character in word:
-    print(word[])
+    index = word.find(character)
+    if len(word) >= index + 3:
+        print(word[index: index + 3])
