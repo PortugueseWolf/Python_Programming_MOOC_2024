0ab6eaf4466995af058e5c43c31777c123762bdf
PortugueseWolf <125361522+PortugueseWolf@users.noreply.github.com>
2024-09-20 09:01:17 +0000

    Done

--- IntroductionToPrograming/Part3/21_find_all_the_substrings/find_all_substrings.py
+++ IntroductionToPrograming/Part3/21_find_all_the_substrings/find_all_substrings.py
@@ -5,7 +5,7 @@ if char in word:
     while True:
         index = word.find(char)
         #print(index)
-        if index >= len(word) - 3 or index < 0:
+        if index >= len(word) - 2 or index < 0:
             break
         print(word[index:index + 3])
         word = word[index + 1:]
