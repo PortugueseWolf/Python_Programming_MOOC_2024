c223bbd6cf0b411d393d7f5f2a7fe67e9df1057d
PortugueseWolf <danieljdferreira@hotmail.com>
2024-10-29 18:28:54 +0000

    Done

--- IntroductionToPrograming/Part05/23_create_a_tuple/create_tuple.py
+++ IntroductionToPrograming/Part05/23_create_a_tuple/create_tuple.py
@@ -1,1 +1,8 @@
 def create_tuple(x: int, y: int, z: int) -> tuple:
+    list = [x, y, z]
+    list.sort()
+    tuple = (list[0], list[2], sum(list))
+    return tuple
+
+if __name__ == "__main__":
+    print(create_tuple(5, 3, -1))
