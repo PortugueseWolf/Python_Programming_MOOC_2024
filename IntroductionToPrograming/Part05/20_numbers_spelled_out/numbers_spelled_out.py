b188d48d732653ad47767e0cd24fa6e78a803a21
PortugueseWolf <danieljdferreira@hotmail.com>
2024-10-29 13:42:36 +0000

    Done

--- IntroductionToPrograming/Part05/20_numbers_spelled_out/numbers_spelled_out.py
+++ IntroductionToPrograming/Part05/20_numbers_spelled_out/numbers_spelled_out.py
@@ -1,25 +1,35 @@
 def dict_of_numbers() -> dict:
     dictionary = {}
-    single_digits = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven",8: "eight", 9: "nine"}
+    single_digits = {0: "zero", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven",8: "eight", 9: "nine"}
     teens = {10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 15: "fifteen", 18: "eighteen"}
-    tens = {2: "twenty", 3: "thirty", 5: "fifthy"}
+    tens = {2: "twenty", 3: "thirty", 4: "forty", 5: "fifty", 8: "eighty"}
 
-    for number in range(1, 30):
+    for number in range(0, 100):
         if number < 10:
             dictionary[number] = single_digits[number]
-        if number >= 10 and number <= 13 or number == 15 or number == 18:
+        
+        if number in teens:
             dictionary[number] = teens[number]
+        
         if number == 14 or number >= 16 and number < 20 and number != 18:
             digit = int(str(number)[1])
             dictionary[number] = (single_digits[digit] + "teen")
+        
         if number >= 20:
-            first_digit = int(str(number)[1])
+            first_digit = int(str(number)[0])
             second_digit = int(str(number)[1])
-            if first_digit in tens:
-                dictionary[number] = (tens[first_digit] + )
-      
-
-    print(dictionary)
+            
+            if second_digit == 0 and first_digit in tens:
+                dictionary[number] = (tens[first_digit])
+            elif second_digit == 0:
+                dictionary[number] = (single_digits[first_digit] + "ty")
+            
+            if second_digit != 0:
+                if first_digit in tens:
+                    dictionary[number] = tens[first_digit] + "-" + single_digits[second_digit]
+                else:
+                    dictionary[number] = (single_digits[first_digit] + "ty-" + single_digits[second_digit])
+    return dictionary
 
 if __name__ == "__main__":
     dict_of_numbers()
