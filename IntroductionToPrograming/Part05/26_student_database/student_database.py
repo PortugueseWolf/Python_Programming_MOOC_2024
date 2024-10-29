4e3191264da6bd6332b5646def570321c0d010f8
PortugueseWolf <danieljdferreira@hotmail.com>
2024-10-29 22:29:04 +0000

    Added basic function to add_student and print_student

--- IntroductionToPrograming/Part05/26_student_database/student_database.py
+++ IntroductionToPrograming/Part05/26_student_database/student_database.py
@@ -1,1 +1,17 @@
-def add_student()
+def add_student(student_dictionary: dict, name: str) -> dict:
+    student_dictionary[name] = "0"
+
+def print_student(student_dictionary: dict, name: str) -> None:
+    if name not in student_dictionary:
+        print(name + ": no such person in the database")
+    else:
+        print(name + ":")
+        print(" no completed courses")
+
+if __name__ == "__main__":
+    students = {}
+    add_student(students, "Peter")
+    add_student(students, "Eliza")
+    print_student(students, "Peter")
+    print_student(students, "Eliza")
+    print_student(students, "Jack")
