49fe437db0acc95d71101f94c5f3c3324fb63a57
PortugueseWolf <danieljdferreira@hotmail.com>
2024-10-29 14:01:50 +0000

    Done

---
+++ IntroductionToPrograming/Part05/21_movie_database/movie_database.py
@@ -0,0 +1,9 @@
+def add_movie(database: list, name: str, director: str, year: int, runtime: int) -> dict:
+    dictionary = {"name": name, "director": director, "year": year, "runtime": runtime}
+    database.append(dictionary)
+
+if __name__ == "__main__":
+    database = []
+    add_movie(database, "Gone with the Python", "Victor Pything", 2017, 116)
+    add_movie(database, "Pythons on a Plane", "Renny Pytholin", 2001, 94)
+    print(database)
