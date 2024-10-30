1dc92b5cf809bf02f6dd14c3ba22d23cc0e2baca
PortugueseWolf <danieljdferreira@hotmail.com>
2024-10-30 21:03:51 +0000

    Work in progress

--- IntroductionToPrograming/Part05/27_a_square_of_letters/a_square_of_letters.py
+++ IntroductionToPrograming/Part05/27_a_square_of_letters/a_square_of_letters.py
@@ -1,6 +1,15 @@
 layers = int(input("Layers: "))
 # 64
 outer_limit = layers * 2 - 1
+square = []
+
 
 for index in range(0, outer_limit):
-    print(chr(65 + layers) * (outer_limit - index))
+    if index == 0:
+        line = chr(64 + layers) * outer_limit
+        square.append(line)
+    
+    if index > 0 and index < layers:
+        line = 
+for line in square:
+    print(line)
