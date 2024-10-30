68cbf7b5f69123c531a08587ac901eb79855900a
PortugueseWolf <danieljdferreira@hotmail.com>
2024-10-30 15:34:35 +0000

    Still thinking

--- IntroductionToPrograming/Part05/27_a_square_of_letters/a_square_of_letters.py
+++ IntroductionToPrograming/Part05/27_a_square_of_letters/a_square_of_letters.py
@@ -1,11 +1,8 @@
 number_to_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 layers = int(input("Layers: "))
-
+# 64
 outer_limit = layers * 2 - 1
-line = number_to_letters[layers-1]* outer_limit
-print(line)
 
-for index in range(1, outer_limit):
-    for char in range(1, outer_limit):
-        
+for index in range(0, outer_limit):
+    print(chr(65 + layers) * (outer_limit - index))
