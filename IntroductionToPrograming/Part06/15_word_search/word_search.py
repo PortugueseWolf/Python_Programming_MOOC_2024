f43f1b88b8c29a47c3877d1c89b9eeeeb5af944e
PortugueseWolf <danieljdferreira@hotmail.com>
2024-12-02 14:29:39 +0000

    Word search in progress, half of "." search done

--- IntroductionToPrograming/Part06/15_word_search/word_search.py
+++ IntroductionToPrograming/Part06/15_word_search/word_search.py
@@ -1,16 +1,22 @@
-from xml.etree.ElementInclude import include
-
-
 def find_words(search_term: str) -> list:
     words_found = []
+    dot_index = search_term.find(".")
+    star_index = search_term.find("*")
+
 
     with open("words.txt", "r") as dictionary:
         for line in dictionary:
-            if "." in search_term:
-            
             line = line.strip()
-            if search_term.lower() in line:
-                .include()
+
+            if "." in search_term:
+                if len(line) == len(search_term) and line[:dot_index] == search_term[:dot_index] and line[dot_index+1:] == search_term[dot_index+1:]:
+                    words_found.append(line)
+
+            if "*" in search_term:
+            
+    print(words_found)
     
     return words_found
 
+if __name__ == "__main__":
+    print(find_words("p.ng"))
