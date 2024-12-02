598722d92f241cd1546e22adacc6e22961ef591c
PortugueseWolf <danieljdferreira@hotmail.com>
2024-12-02 20:16:57 +0000

    Word search completed

--- IntroductionToPrograming/Part06/15_word_search/word_search.py
+++ IntroductionToPrograming/Part06/15_word_search/word_search.py
@@ -1,6 +1,9 @@
+from re import L
+
+
 def find_words(search_term: str) -> list:
     words_found = []
-    print(search_term[1:])
+
     with open("words.txt", "r") as dictionary:
         for line in dictionary:
             line = line.strip()
@@ -22,13 +25,17 @@ def find_words(search_term: str) -> list:
             if "*" in search_term:
 
                 if search_term.find("*") == 0:
-                    if line[(len(search_term)-1):] == search_term[1:]:
+                    compare = line[len(line) - len(search_term) + 1:]
+                    if compare == search_term[1:]:
                         words_found.append(line)
                 else:
                     if line[:len(search_term)-1] == search_term[:len(search_term)-1]:
                         words_found.append(line)
+            
+            if line == search_term:
+                words_found.append(line)
     
     return words_found
 
 if __name__ == "__main__":
-    print(find_words("ca*"))
+    print(find_words("*okes"))
