4b4d70605cd7dd9eb23afcd120204ce5fd8e595d
PortugueseWolf <danieljdferreira@hotmail.com>
2024-12-09 18:08:09 +0000

    Done

--- IntroductionToPrograming/Part07/06_password_generator_part2/password_generator_part2.py
+++ IntroductionToPrograming/Part07/06_password_generator_part2/password_generator_part2.py
@@ -1,31 +1,64 @@
-from random import randint
+from random import randint, sample
 
 def generate_strong_password(x: int, has_numbers: bool, has_symbols: bool) -> str:
     characters = ""
     password = ""
-    guaranteed_leter = randint(0, x)
-    guaranteed_number = randint(0, x)
-    guaranteed_symbol = randint(0, x)
 
     for i in range(97, 123):
         characters += chr(i)
 
-    if has_numbers:
-        for i in range(48, 57):
-            characters += chr(i)
-            
+    characters += "!?=+-()#"
+    
+    for i in range(48, 58):
+        characters += chr(i)
+    
+    if not has_numbers and not has_symbols:
+        new_password = sample(characters[:26], x)
 
+        for i in new_password:
+            password += i
         
-    if has_symbols:
-        characters += "!?=+-()#"
+        return password
 
-    for i in range(x):
-        password += characters[randint(0,len(characters) - 1)]
+    if has_numbers and has_symbols:
+        new_password = sample(characters, x)
 
-    if has_numbers
+        guaranteed_spots = sample(range(0,x - 1), 3)
+
+        new_password[guaranteed_spots[0]] = characters[randint(0, 26)]     
+        new_password[guaranteed_spots[1]] = characters[randint(26, 33)]
+        new_password[guaranteed_spots[2]] = characters[randint(33, 43)]
+        for i in new_password:
+            password += str(i)
+
+        return password
+
+    if has_symbols and not has_numbers:
+        new_password = sample(characters[:33], x)
+
+        guaranteed_spots = sample(range(0,x - 1), 2)
+
+        new_password[guaranteed_spots[0]] = characters[randint(0, 26)]      
+        new_password[guaranteed_spots[1]] = characters[randint(26, 33)]
+
+        for i in new_password:
+            password += str(i)
+
+        return password
+
+    characters = characters[:26] + characters[34:]
+
+    new_password = sample(characters, x)
+
+    guaranteed_spots = sample(range(0,x - 1), 2)
+    new_password[guaranteed_spots[0]] = characters[randint(0, 26)] 
+    new_password[guaranteed_spots[1]] = characters[randint(26, 35)]
+
+    for i in new_password:
+        password += str(i)
 
     return password
 
 if __name__ == "__main__":
-    for i in range(10):
-        print(generate_strong_password(5, False, True))
+    for i in range(1000):
+     print(generate_strong_password(6, True, True))
